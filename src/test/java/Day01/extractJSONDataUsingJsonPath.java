afe9508109cf163f6365adab1c66d62d21fabb16
suraj <surajgit12@gmail.com>
2025-06-20 15:49:20 +0530

    removed text

--- src/test/java/Day01/extractJSONDataUsingJsonPath.java
+++ src/test/java/Day01/extractJSONDataUsingJsonPath.java
@@ -73,11 +73,8 @@ public class extractJSONDataUsingJsonPath {
 				assertThat(response.path("[0].name"), equalTo("suraj"));
 				assertThat(response.path("[0].courses[1]"), equalTo("selenium"));
 	
-	              // Assert is equal is new textewfewfewfwfwjdrerwe4gege
-				//sgerkjgrekbver
-				
-				//1234
-		//6578
+	
+
 				
 	}}
 		
